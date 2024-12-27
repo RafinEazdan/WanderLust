a4a3445f2f6da408e5ed12d1299803f214181be6
Eazdan Mostafa Rafin <eazdan.rafin@gmail.com>
2024-12-27 12:23:50 +0600

    for publish the webiste

--- api/index.js
+++ api/index.js
@@ -28,7 +28,8 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://WanderLust.vercel.app" ||"http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
